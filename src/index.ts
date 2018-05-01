b0d360afdba9e10afd4fd6187d04bd30f12e93d5
Sun1ive <sunliveua@gmail.com>
2018-05-01 12:09:30 +0300

    123

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,14 @@
 import * as express from 'express';
 import * as cors from 'cors';
 import * as morgan from 'morgan';
+import config from './config';
 
 const app = express();
 app.use(cors());
 app.use(morgan('combined'));
 
+app.get('/', (req, res) => {
+  res.send('Hello world');
+});
 
-app.listen(8081, () => console.log('Server on 111'));
+app.listen(config.port, () => console.log('Server on ', config.port));
