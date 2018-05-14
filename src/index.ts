9eb2592ed1382ae491c5a8c12a1782a659053db4
sun1ive <sunliveua@gmail.com>
2018-05-14 15:21:13 +0300

    gettin better

--- src/index.ts
+++ src/index.ts
@@ -4,11 +4,14 @@ import * as morgan from 'morgan';
 import config from './config';
 
 const app = express();
+
 app.use(cors());
 app.use(morgan('combined'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('Hello world');
 });
 
-app.listen(config.port, () => console.log('Server on ', config.port));
+app.listen(config.port, () => console.log('Server is running at port', config.port));
